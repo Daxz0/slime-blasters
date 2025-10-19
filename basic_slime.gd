431e3c40677d71db3cfd085767fcc0b6edf15c6a
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-10-19 16:52:08 -0700

    + trees and things

--- basic_slime.gd
+++ basic_slime.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 
+
+@onready var player = get_node("/root/Game/Player")
+
 const SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
-	   
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * SPEED
+
+	move_and_slide()

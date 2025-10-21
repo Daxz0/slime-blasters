cc5f2c09b4d9606f906f8fac49613accbf3d0b0e
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-10-20 18:12:36 -0700

    merge

--- game.gd
+++ game.gd
@@ -22,6 +22,7 @@ func _on_player_health_depleted() -> void:
     %GameOver.visible = true
     get_tree().paused = true
 
-func _on_start_button_pressed() -> void:
+
+func _on_button_pressed() -> void:
     get_tree().paused = false
     %GameStart.visible = false
